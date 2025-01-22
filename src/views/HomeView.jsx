285fe3be553a5225cea32ebe052d6210d056f5d7
m330001 <duswlsdl789@naver.com>
2025-01-22 17:23:39 +0900

    두번째 커밋 입니다.

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Skip from '../components/Skip'
+import Header from '../components/Header'
+import Intro from '../components/Intro'
+import Skill from '../components/Skill'
+import Site from '../components/Site'
+import Port from '../components/Port'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Main from '../components/Main'
 
 const HomeView = () => {
   return (
-    <div>HomeView</div>
+    <>
+        <Skip />
+        <Header />
+        <Main >
+          <Intro />
+          <Skill />
+          <Site />
+          <Port />
+          <Contact />
+        </Main>
+        <Footer />
+    </>
   )
 }
 
